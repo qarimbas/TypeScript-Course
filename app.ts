fa8776d1618db65938a1a33d576fa9ac51b37b35
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2022-07-11 10:14:12 +0300

    basics

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-    const person = {
-        name: 'Kerem',
-        age: 23
-      };
-      
-      console.log(person.name);
-      
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
+
+generateError('An error occurred!', 500);
