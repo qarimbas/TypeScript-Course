cf2a38ae5556fc2b34983dd3f5d7508d386d6548
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2022-07-16 14:25:02 +0300

    classes

--- app.ts
+++ app.ts
@@ -1,15 +1,46 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-  userName = userInput;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
 }
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
 }
 
-generateError('An error occurred!', 500);
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name?: string;
+  age = 30;
+
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person();
+// user1.name = 'Manu';
+
+user1.greet('Hi there - I am');
+console.log(user1);
